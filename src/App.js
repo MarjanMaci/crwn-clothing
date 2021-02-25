c2ee6c4409c978e8989b7ec2a078b1dcadbacec0
unknown <marjanmarjanvoski13@gmail.com>
2021-02-25 19:29:59 +0100

    Vnesen shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+
+import {Route, Switch} from 'react-router-dom'
+
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component'
 
+import ShopPage from '../src/pages/shop/shop.component'
+
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
